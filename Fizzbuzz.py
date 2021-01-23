de79f23a50ed5842671915eb826f8bb5fd417c9b
Charlie <charlesrichardson123000@gmail.com>
2021-01-23 14:06:44 -0600

    Fizzbuzz with user input

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,6 +1,7 @@
-class Answer():
+class Solution():
     def FizzBuzz(self):
-        for i in range(1,101):
+        x=self.getInput()
+        for i in range(1,x):
             if(i%15==0):
                 print('FizzBuzz')
             elif(i%3==0):
@@ -9,5 +10,16 @@ class Answer():
                 print('Buzz')
             else:
                 print(i)
-My=Answer()
-My.FizzBuzz()
+    def getInput(self):
+        while(True):
+            i= input('Enter the number of ints you want: ')
+            if (i.isalpha()):
+                print('Error!\nTry again, positive intergers above 0 only!')
+                continue
+            elif(int(i)<=0):
+                print('Error!\nTry again, positive intergers above 0 only!')
+                continue
+            else:
+                return int(i)+1
+myAnswer=Solution()
+myAnswer.FizzBuzz()
